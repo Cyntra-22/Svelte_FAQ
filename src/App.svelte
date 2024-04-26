181f14e57e8a82ac6e81ea63d1e120134994a541
Cyntra-22 <140875968+Cyntra-22@users.noreply.github.com>
2024-04-26 16:25:23 +0700

    Added the script part

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,15 @@
 <script>
-	
+	let faqItems = [
+    { question: "What is Frontend Mentor, and how will it help me?", answer: "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.", isOpen: false },
+    { question: "Is Frontend Mentor free?", answer: "Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels. ", isOpen: false },
+    { question: "Can I use Frontend Mentor projects in my portfolio?", answer: "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers! ", isOpen: false },
+	{ question: " How can I get help if I'm stuck on a Frontend Mentor challenge?", answer: "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.", isOpen: false },
+  ];
+  const src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.wallpaperflare.com%2Fsearch%3Fwallpaper%3Dpurple%2Blandscape&psig=AOvVaw2j8HnSoQQYbpDXdW2cVUcV&ust=1714155944777000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLjG-9P-3YUDFQAAAAAdAAAAABAE"
+
+  function toggleAnswer(index) {
+    faqItems[index].isOpen = !faqItems[index].isOpen;
+  }
 </script>
 
 <style>

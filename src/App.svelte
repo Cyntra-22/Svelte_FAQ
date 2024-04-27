f3fe1373f35ff7dd6b780b77ad8cb0ac7ae815ac
Cyntra-22 <140875968+Cyntra-22@users.noreply.github.com>
2024-04-27 15:28:37 +0700

    Seperated a new component for FAQ

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,5 @@
+<!--
+
 <script>
 	import { onMount } from 'svelte';
 	let faqItems = [
@@ -91,8 +93,6 @@
 		padding-right: 10px;
 	}
 
-
-
 	.question-style{
 		
 		font-weight: bold;
@@ -178,4 +178,77 @@
 		</div>
 	</div>
 
-</body>
+</body> 
+-->
+
+<script>
+  import FAQItem from './FAQItem.svelte';
+
+  let faqItems = [
+    { question: "What is Frontend Mentor, and how will it help me?", answer: "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.", isOpen: false },
+    { question: "Is Frontend Mentor free?", answer: "Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels. ", isOpen: false },
+    { question: "Can I use Frontend Mentor projects in my portfolio?", answer: "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers! ", isOpen: false },
+    { question: " How can I get help if I'm stuck on a Frontend Mentor challenge?", answer: "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.", isOpen: false },
+  ];
+</script>
+
+<style>
+ 
+  .main-container {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: hsl(0, 0%, 100%);
+    padding: 20px;
+    border-radius: 10px;
+    width: 50%;
+    box-shadow: 0 6px 6px rgba(0, 0, 0, 0.1);
+  }
+
+	.faq-container {
+    padding-bottom: 10px;
+    border-bottom: 1px solid rgb(169, 165, 165);
+  }
+
+  .faq-header {
+    display: flex;
+  }
+
+  h1 {
+    margin: 0;
+    padding: 0;
+    font-size: 38px;
+  }
+
+               
+
+  @media (min-width: 1080px) {
+    .main-container {
+      width: 30%;
+    }
+  }
+
+  @media (max-width: 768px) {
+    h1 {
+      font-size: 29px;
+    }
+  }
+</style>
+
+<div class="main-container">
+  <div class="faq-header">
+    <img src="/icon-star.svg" alt="faq-title"/>
+    <h1>FAQs</h1>
+  </div>
+
+  {#each faqItems as faqItem, index}
+  	<div class="faq-container" style="{index === faqItems.length - 1 ? 'border-bottom: none;' : ''}">
+		<FAQItem question={faqItem.question} answer={faqItem.answer} isOpen={faqItem.isOpen}/>
+	</div>
+  {/each}
+
+</div>
+
+
+
---
+++ src/FAQItem.svelte
@@ -0,0 +1,62 @@
+<script>
+  export let question;
+  export let answer;
+  export let isOpen;
+
+  function toggleAnswer() {
+    isOpen = !isOpen;
+  }
+</script>
+
+<style>
+  
+
+
+  button {
+    background: none;
+    border: none;
+    cursor: pointer;
+  }
+
+  .alignment {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .question-style {
+    font-weight: bold;
+    font-size: 16px;
+    color: black;
+  }
+
+  .question-style:hover {
+    color: var(--text-colorAns);
+  }
+
+  .answer-style {
+    color: var(--text-colorAns);
+    font-size: 16px;
+  }
+</style>
+
+
+  <div class="alignment">
+    <div class="question-style" >
+      <span>{question}</span>
+    </div>
+    <div>
+      <button class="icon-style" on:click={toggleAnswer}>
+        {#if isOpen}
+          <img src="/icon-minus.svg" alt="minus icon"/>
+        {:else}
+          <img src="/icon-plus.svg" alt="plus icon"/>
+        {/if}
+      </button>
+    </div>
+  
+  {#if isOpen}
+    <p class="answer-style">{answer}</p>
+  {/if}
+</div>
